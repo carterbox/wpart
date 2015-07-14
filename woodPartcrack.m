1d070fcf1ad324ea37957402cfb0915f1368b433
carterbox <daniel.j.ching@gmail.com>
2015-07-14 13:51:53 -0700

    Deallocates the massive stack and the reloads each smaller stack and maps them separately.

--- woodPartcrack.m
+++ woodPartcrack.m
@@ -15,45 +15,34 @@ numworkers = 6;
 start_time = tic;
 mkdir(OUTDIR);
 logfile = fopen([OUTDIR '/log.txt'],'a');
+diary([OUTDIR '/log.txt']);
 fprintf(logfile,['\n' datestr(datetime('now'))]);
 
 %% Gather all the images -------------------------------------------------
 if size(gcp) == 0, p = parpool(numworkers); else p = gcp; end
 
+INDIR{1} = '/media/OCT14M/Segmentations/recon_proj_74';
+INDIR{2} = '/media/OCT14M/Segmentations/recon_proj_75';
+INDIR{3} = '/media/OCT14M/Segmentations/recon_proj_76';
+INDIR{3} = '/media/OCT14M/Segmentations/recon_proj_77';
+INDIR{5} = '/media/OCT14M/Segmentations/recon_proj_78';
+INDIR{6} = '/media/OCT14M/Segmentations/recon_proj_79';
+
 stack(6*STACKDEPTH,HEIGHT,WIDTH) = uint8(0); %sprintf('uint%i', kBITDEPTH))
+diary on;
 for key = 1:6
-
-switch key
-    case 1
-         INDIR = '/media/OCT14M/Segmentations/recon_proj_74';
-    case 2
-         INDIR = '/media/OCT14M/Segmentations/recon_proj_75';
-    case 6
-         INDIR = '/media/OCT14M/Segmentations/recon_proj_76';
-    case 3
-         INDIR = '/media/OCT14M/Segmentations/recon_proj_77';
-    case 4
-         INDIR = '/media/OCT14M/Segmentations/recon_proj_78';
-    case 5
-         INDIR = '/media/OCT14M/Segmentations/recon_proj_79';
+addpath(genpath(INDIR{key})); % Files need on searchpath to use.
+lo = (key-1)*STACKDEPTH + 1; hi = key*STACKDEPTH;
+stack(:,:,lo:hi) = imstackload([ INDIR{key} '/subset' ],sprintf('uint%i', kBITDEPTH));
 end
-
-addpath(genpath(INDIR)); % Files need on searchpath to use.
-
-lo = (key-1)*STACKDEPTH + 1; 
-hi = key*STACKDEPTH;
-stack(:,:,lo:hi) = imstackload([ INDIR '/subset' ],sprintf('uint%i', kBITDEPTH));
-
-end
+diary off;
 
 delete(p);
 
 %% Finding the gaussian distribution mixture -----------------------------
 
 labels = findThresholds(stack, kNUMGDISTS, kBITDEPTH, logfile);
-% if(~input('Does this distribution look appropriate? (1 Yes / 0 No)\n'))
-%     return;
-% end
+clear stack;
 disp('Saving labels ...');
 save([OUTDIR '/labels.mat'], 'labels');
 print([OUTDIR '/mixedgaussians'], '-dpng');
@@ -61,23 +50,24 @@ print([OUTDIR '/mixedgaussians'], '-dpng');
 %% Segmenting and Smoothing ----------------------------------------------
 if size(gcp) == 0, p = parpool(numworkers); else p = gcp; end
 
-% Segment the image according to the lookup-table.
-% fprintf('Mapping...\n');
-% segmented = labels(stack + 1);
-segmented = woodmap(stack, labels);
+for key = 1:6
+    % Load each of the stacks to process them separately
+    stack = imstackload([INDIR{key} '/subset'],...
+                        sprintf('uint%i', kBITDEPTH));
+    
+    % Segment the image according to the lookup-table.
+    % fprintf('Mapping...\n');
+    % segmented = labels(stack + 1);
+    segmented = woodmap(stack, labels);
 
-% Save a copy for debugging.
-raw_segmented = segmented;
-save([OUTDIR '/raw_segmented.mat'],'raw_segmented');
-clear raw_segmented;
+    segmented = removeislands(segmented, 5, 80);
+    output = woodcolor('c', segmented, 5, logfile, 1, stack);
 
-segmented = removeislands(segmented, 5, 80);
-output = woodcolor('c', segmented, 5, logfile, 1, stack);
+    imstacksave(output,sprintf('%s/segmented_c%2i',OUTDIR,key),samplename);
+end
 
-imstacksave(output,[ OUTDIR '/segmented_c' ],samplename);
 print([OUTDIR '/comparison'],'-dpng');
 fprintf(logfile,'\n');
 fprintf(logfile,'Total runtime was %.2f\n',toc(start_time));
 
 delete(p); fclose(logfile);
-end
