4033dba1d8035f56792194950b5fc98f8a871cf5
carterbox <daniel.j.ching@gmail.com>
2015-07-30 18:46:18 -0700

    added OBJ creating algo to the visualization section. Then realized that it my 1024x512x1600 volume was too large for the internet as an OBJ file.

--- woodPartcrack.m
+++ woodPartcrack.m
@@ -29,7 +29,7 @@ INDIR{3} = '/media/OCT14M/Segmentations/Chad/recon_proj_76';
 INDIR{4} = '/media/OCT14M/Segmentations/Chad/recon_proj_77';
 INDIR{5} = '/media/OCT14M/Segmentations/Chad/recon_proj_78';
 INDIR{6} = '/media/OCT14M/Segmentations/Chad/recon_proj_79';
-
+%% ---------------------
 stack(HEIGHT,WIDTH,STACKDEPTH,NUMSTACKS) = uint8(0); %sprintf('uint%i', kBITDEPTH))
 diary on;
 parfor key = 1:6
@@ -40,7 +40,6 @@ end
 diary off;
 
 delete(p);
-
 
 % Sample 2 percent of the data to reduce memory and processing consumption.
 fprintf('Sampling dataset... \n');
@@ -54,7 +53,7 @@ sample = double(sample(:));
 %% Finding the gaussian distribution mixture -----------------------------
 
 labels = findThresholds(sample, kNUMGDISTS, kBITDEPTH, logfile);
-clear stack;
+clear sample;
 disp('Saving labels ...');
 save([OUTDIR '/labels.mat'], 'labels');
 print([OUTDIR '/mixedgaussians'], '-dpng');
@@ -62,20 +61,21 @@ print([OUTDIR '/mixedgaussians'], '-dpng');
 %% Segmenting and Smoothing ----------------------------------------------
 if size(gcp) == 0, p = parpool(numworkers); else p = gcp; end
 
-for key = 1:6
+parfor key = 1:6
     % Load each of the stacks to process them separately
     stack = imstackload([INDIR{key} '/subset'],...
                         sprintf('uint%i', kBITDEPTH));
     
     % Segment the image according to the lookup-table.
-    % fprintf('Mapping...\n');
-    % segmented = labels(stack + 1);
-    segmented = woodmap(stack, labels);
+    fprintf('Mapping...\n');
+    segmented = labels(stack + 1);
+    %segmented = woodmap(stack, labels);
 
     segmented = removeislands(segmented, 5, 80);
     output = woodcolor('b', segmented, 5, logfile, 1, stack);
 
     imstacksave(output,sprintf('%s/segmented_c%2i',OUTDIR,key),samplename);
+    %makeobj(segmented, sprintf('%s/step%2i.obj', OUTDIR, key));
 end
 
 print([OUTDIR '/comparison'],'-dpng');
