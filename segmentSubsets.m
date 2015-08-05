785eeb916755952634ac606daa94141171772829
carterbox <daniel.j.ching@gmail.com>
2015-08-04 17:57:55 -0700

    data needs to cover the entire range for newer distribution sorting algo to be reliable.

--- segmentSubsets.m
+++ segmentSubsets.m
@@ -48,7 +48,7 @@ sample = double(sample(:));
 
 diary off;
 %% Finding the gaussian distribution mixture -----------------------------
-
+sample = sample(rescale(sample, kBITDEPTH, logfile));
 labels = findThresholds(sample, kNUMGDISTS, kBITDEPTH, logfile);
 clear sample;
 disp('Saving labels ...');
