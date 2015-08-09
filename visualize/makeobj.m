b34a753257986fcf6a42acab26e1a785a2fc82d1
carterbox <daniel.j.ching@gmail.com>
2015-08-08 20:51:16 -0700

    volume is meshed before OBJ is created. Drops file size down by an order of magnitude (now ~50MB) however, that's still larger than the 6MB for the prerendered 3D rotation.

--- visualize/makeobj.m
+++ visualize/makeobj.m
@@ -12,9 +12,16 @@ function makeobj( segmentedvol, filenamedir )
 % OUTPUTS
 %
 %% -----------------------------------------------------------------------
-[x0,y0,z0] = size(segmentedvol);
+%% Find the isosurface ---------------------------------------------------
+OBJ = cell(5,1);
 
-%% Get the Coordinates of each vertex ------------------------------------
+for phase = 3:5
+    OBJ{phase} = isosurface(segmentedvol, phase, 'verbose');
+end
+
+%% Scaling and centering the vertices ------------------------------------
+
+[x0,y0,z0] = size(segmentedvol);
 
 % Centering the object.
 dx = -(x0 - 1)/2 - 1;
@@ -22,16 +29,18 @@ dy = -(y0 - 1)/2 - 1;
 dz = -(z0 - 1)/2 - 1;
 scale = max([x0;y0;z0]);
 
-OBJ = cell(5,1);
 for phase = 3:5
-    binaryvol = segmentedvol == phase;
-    num_vertices = sum(binaryvol(:));
-    if num_vertices > 0
-        % Get the cartesian coordinates of all pixels in this phase.
-        [x,y,z] = ind2sub([x0,y0,z0],find(binaryvol));
-        
-        OBJ{phase}.vertices_point = cat(2,x+dx,y+dy,z+dz)./scale;
-    end
+if ~isempty(OBJ{phase}) && ~isempty(OBJ{phase}.vertices)
+    tempvertexarray = OBJ{phase}.vertices;
+    
+    tempvertexarray(:,1) =  tempvertexarray(:,1) + dx;
+    tempvertexarray(:,2) =  tempvertexarray(:,2) + dy;
+    tempvertexarray(:,3) =  tempvertexarray(:,3) + dz;
+    
+    tempvertexarray = tempvertexarray./scale;
+    
+    OBJ{phase}.vertices = tempvertexarray;
+end
 end
 
 %% Write the OBJ to file -------------------------------------------------
@@ -39,23 +48,29 @@ fid = fopen(filenamedir,'w');
 names = {'air','shadow','wood','interphase','adhesive'};
 
 for phase = 1:length(OBJ)
-if ~isempty(OBJ{phase})
-    
-        V = OBJ{phase}.vertices_point'; disp(size(V,1));
-        fprintf('\nSaving object: %s ...', names{phase});
+if ~isempty(OBJ{phase}) && ~isempty(OBJ{phase}.vertices)
+        fprintf('\nSaving object: %s ...', names{phase}); %tic
+        fprintf(fid,'o %s\n', names{phase});
         
         % Generate a string
-        tic
-        assert(size(V,1) == 3);
-        string0 = sprintf('o %s\n', names{phase});
-        string1 = sprintf('v %5.5f %5.5f %5.5f\n', V);
-        
+        verts = OBJ{phase}.vertices'; %disp(size(verts,1));
+        %assert(size(verts,1) == 3);
+        string1 = sprintf('v %5.5f %5.5f %5.5f\n', verts);
         % Save string to file all at once.
-        fprintf(fid,'%s',string0);
         fprintf(fid,'%s',string1);
-        disp(toc)
-        fprintf(' DONE.');
+        clear string1 verts
         
+        % Generate a string
+        faces = OBJ{phase}.faces'; %disp(size(faces,1));
+        %assert(size(faces,1) == 3);
+        string2 = sprintf('f %i %i %i\n', faces);
+        % Save string to file all at once.
+        fprintf(fid,'%s',string2);
+        clear string2 faces
+        
+        %disp(toc)
+        fprintf(' DONE.\n');
+        fprintf(fid,'\r\n');
 end
 end
 fclose(fid);
