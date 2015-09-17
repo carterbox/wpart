2ffd5db8971c47d20a8d2d96927e7940d09dc6f5
carterbox <daniel.j.ching@gmail.com>
2015-09-17 10:24:11 -0700

    Added imnamestack check for whether directory exists

--- preprocess/imnamestack.m
+++ preprocess/imnamestack.m
@@ -15,41 +15,45 @@ function [namestack, count] = imnamestack(directory, notch, depth)
 kEXTENSION = {'.tif', '.png', '.tiff'}; % The filetype to be loaded.
 
 %% -----------------------------------------------------------------------
-% Get the details of all the files in DIRECTORY.
-fcontents = dir(directory);
-fprintf('Loading filenames from %s ... ', directory);
-% Skip the first two entries (./ and ../) and determine if DIRECTORY is
-% empty.
-start = 1;
-while fcontents(start).isdir == 1
-    start = start + 1;
-    if start > length(fcontents), error('There are no files there!'); end
-end
 
-% Preallocate a cell for the names.
-namestack = cell(depth,1);
+if exist(directory,'dir')
+    % Get the details of all the files in DIRECTORY.
+    fcontents = dir(directory);
+    fprintf('Loading filenames from %s ... ', directory);
+    % Skip the first two entries (./ and ../) and determine if DIRECTORY is
+    % empty.
+    start = 1;
+    while fcontents(start).isdir == 1
+        start = start + 1;
+        if start > length(fcontents), error('There are no files there!'); end
+    end
 
-% Loads all the names into the cell.
-count = 0;
-stop = min((start + notch - 16), length(fcontents));
-start = max(start, start + (notch - 15 - depth));
-for i = start:stop
-    % Check that the entry is not a directory.
-    if(~fcontents(i).isdir)
-        [~,~,ext] = fileparts(fcontents(i).name);
-        % Check for the desired filetype.
-        if strcmp(ext, kEXTENSION{1}) || strcmp(ext, kEXTENSION{2}) || strcmp(ext, kEXTENSION{3})
-            count = count + 1;
-            namestack{count} = fcontents(i).name;
+    % Preallocate a cell for the names.
+    namestack = cell(depth,1);
+
+    % Loads all the names into the cell.
+    count = 0;
+    stop = min((start + notch - 16), length(fcontents));
+    start = max(start, start + (notch - 15 - depth));
+    for i = start:stop
+        % Check that the entry is not a directory.
+        if(~fcontents(i).isdir)
+            [~,~,ext] = fileparts(fcontents(i).name);
+            % Check for the desired filetype.
+            if strcmp(ext, kEXTENSION{1}) || strcmp(ext, kEXTENSION{2}) || strcmp(ext, kEXTENSION{3})
+                count = count + 1;
+                namestack{count} = fcontents(i).name;
+            end
         end
     end
-end
-fprintf('LOADED %i NAMES\n', count);
+    fprintf('LOADED %i NAMES\n', count);
 
-if(count ~= depth)
-    warning('Matrix size does not match desired depth.');
-    namestack = namestack(1:count);
-end
+    if(count ~= depth)
+        warning('Matrix size does not match desired depth.');
+        namestack = namestack(1:count);
+    end
+else
+    error('%s does not exist!', directory);
 end
 
 % TODO: Sort the files by name incase the OS doesn't return the filenames
