f0992e02e1f32c446ceea3723b11b2953f2fb69e
carterbox <daniel.j.ching@gmail.com>
2016-03-18 18:58:25 -0700

    Updated rescale and mapToColor functions.

--- preprocess/rescale.m
+++ preprocess/rescale.m
@@ -3,14 +3,19 @@ function stack = rescale(stack, bitdepth, logfile, lowerthresh, upperthresh)
 %   Values are scaled so the largest value become maxint of BITDEPTH and the
 %   smallest value becomes minint of BITDEPTH.
 %
-% version 1.1.0
+% version 1.1.1
 % INPUTS
 %   stack: an image stack to be converted
 %   bitdepth (double): the number of bits to fit the data into.
 %   logfile: the identifier of a logfile 1 is the default.
+%   lowerthresh:
+%   upperthresh:
 %
 % OUTPUTS
 %   stack (bitdepth): the rescaled stack
+%
+% version 1.1.1 - Properly scale values outside the range
+% [lowerthresh,upperthresh] to 0 and MAXINT.
 %
 %% ----------------------------------------------------------------------------
 
@@ -21,16 +26,23 @@ if(nargin) < 5, upperthresh = max(stack(:)); end
 large = upperthresh;
 
 if(nargin) < 4, lowerthresh = min(stack(:)); end
-small = max(min(stack(:)),lowerthresh);
-fprintf(logfile, '\nOLD MAX: %.1f   OLD MIN: %.1f \n', large, small);
+small = lowerthresh;
+
+fprintf(logfile, '\nOLD MIN: %.1f   OLD MAX: %.1f \n', small, large);
 
 if large ~= double(2^bitdepth - 1) || small ~= 0
     % Rescale the values
-    stack = (stack - small)./(large - small) * double(2^bitdepth - 1);
+    stack = (stack - small)./(large - small);
+    
+    % Set any values outside the range to zero and MAXINT.
+    stack(stack < 0) = 0.0;
+    stack(stack > 1) = 1.0;
+    
+    stack = stack .* double(2^bitdepth - 1);  
 
     % Log the new min and max values.
-    fprintf(logfile, 'NEW MAX: %.1f   NEW MIN: %.1f \n',...
-            max(stack(:)), min(stack(:)));
+    fprintf(logfile, 'NEW MIN: %.1f   NEW MAX: %.1f \n',...
+            min(stack(:)), max(stack(:)));
 end
 
 % Change the class of stack to match desired bitdepth
