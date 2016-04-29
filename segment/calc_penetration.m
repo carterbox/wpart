f3dbcfd591f6550329ec0610b7ec76be5899d19e
carterbox <daniel.j.ching@gmail.com>
2016-04-29 15:39:50 -0700

    Fixed EP and WP calculation routines so they work correctly and so the graphics are displayed better.

--- segment/calc_penetration.m
+++ segment/calc_penetration.m
@@ -1,6 +1,6 @@
 function [EP, WP] = calc_penetration(volume, points)
 %CALC_PENETRATION adhesive penetration characterizer.
-% version 0.0.0
+% version 0.0.1
 %
 %INPUTS
 % VOLUME (boolean): A boolean volume where true values are the locations of
@@ -14,6 +14,8 @@ function [EP, WP] = calc_penetration(volume, points)
 % accounts for both mass and perpendicular distance. Masses farther away
 % from the bondline count more.
 %
+% version 0.0.1 - fixed calculations so they operate on XZ planes, and
+% fixed graphics so they show at the proper scale.
 %% TEST CASE
 
 %points = [1,1,1;0,0,1;1,0,1];
@@ -22,20 +24,27 @@ function [EP, WP] = calc_penetration(volume, points)
 %% ------------------------------------------------------------------------
 
 % Generate a best fit plane from the points and plot it to check.
-[normal, ~, point] = affine_fit(points);
+[normal, V, point] = affine_fit(points);
 
 h = figure(1); clf(h); hold on; daspect([1 1 1]);
 % plot the points
 scatter3(points(:,1),points(:,2),points(:,3));
 % plot a marker on plane and normal vector
-plot3(point(1),point(2),point(3),'ro','markersize',15,...
+plot3(point(1),point(2),point(3),'ro','markersize',5,...
       'markerfacecolor','red');
-quiver3(point(1),point(2),point(3),normal(1)/3,normal(2)/3,normal(3)/3,...
-        'r','linewidth',2)
+quiver3(point(1),point(2),point(3),100*normal(1),100*normal(2),100*normal(3),...
+        'r','linewidth',2, 'AutoScale','off');
 %plot the fitted plane
-[X,Y] = meshgrid(linspace(0,1,3));
-surf(X,Y, -(normal(1)/normal(3)*X + normal(2)/normal(3)*Y -...
-     dot(normal,point)/normal(3)),'facecolor','red','facealpha',0.5);
+xmin = min(points(:,1));
+xmax = max(points(:,1));
+zmin = min(points(:,3));
+zmax = max(points(:,3));
+
+[X,Z] = meshgrid(linspace(xmin,xmax,3),linspace(zmin,zmax,3));
+
+surf(X, -(normal(1)/normal(2)*X + normal(3)/normal(2)*Z -...
+     dot(normal,point)/normal(2)), Z,'facecolor','red','facealpha',0.5);
+%surf(X,repmat(point(2),size(X)),Z);
 
 % For each TRUE value in volume calculate the WP and WP
 EP = effective_penetration(volume,normal,point);
@@ -48,37 +57,38 @@ function EP = effective_penetration(volume,normal,point)
 
 % Calculate the area of the plane. The plane normal contains a z
 % component. Otherwise, this calculated area will be infinite.
-if(normal(3) == 0), error('The bondline is too straight.' + ...
+if(normal(2) == 0), error('Bondline should horizontal in slices!' + ...
                           ' Caclulated area is infinite!'); end
 
+                  
+fprintf('\nCalculating EP...');
 %z = @(x,y) (dot(normal,point) - (normal(1)*x + normal(2)*y)) / normal(3);
 %area = integral2(z,0,1,0,1);
-[xmax,ymax,~] = size(volume);
-area = sqrt((-normal(1)/normal(3)).^2 + (-normal(1)/normal(3)).^2 + 1) ...
-       * (xmax - 1) * (ymax - 1);
+[xmax,~,zmax] = size(volume);
+area = sqrt((-normal(1)/normal(2)).^2 + (-normal(3)/normal(2)).^2 + 1) ...
+       * (xmax - 1) * (zmax - 1);
 
 % Literally, take the sum of the number of adhesive voxels (volume)
 % and divide by the area of the bondline (area).
 EP = sum(volume(:))/area;
-
+fprintf(' %f', EP);
 end
 
 function WP = weighted_penetration(volume,normal,point)
 % Calculates the weighted penetration of the points in the volume from the
 % line defined by a normal and a point.
+fprintf('\nCalculating WP...');
 
-    WP = 0;
-
-    for i = 1:numel(volume)
-    if(volume(i))
-        [x,y,z] = ind2sub(size(volume),i);
-        distance = plane_to_point(normal,point,[x,y,z]);
-        WP = WP + distance^2;
-    end
-    end
-
-    WP = sqrt(WP/sum(volume(:)));
-
+    trus = find(volume);
+    [x,y,z] = ind2sub(size(volume),trus); clear trus;
+    
+    normal = normal/norm(normal);
+    
+    distance = (x-point(1))*normal(1) + (y-point(2))*normal(2) + (z-point(3))*normal(3);
+    
+    WP = sqrt( sum(distance.^2) / sum(volume(:)));
+    
+    fprintf(' %f\n', WP);
 end
 
 function d = plane_to_point(normal,A,B)
